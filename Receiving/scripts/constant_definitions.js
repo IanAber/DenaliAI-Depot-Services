1ada1105995053fe5da5f4b5d7a2bee4e2e154a9
IanAber <Ian.Abercrombie@CedarTechnology.com>
2021-02-22 12:06:15 -0500

    Add files via upload

---
+++ Receiving/scripts/constant_definitions.js
@@ -0,0 +1,27 @@
+// Cherwell API Cient Key
+const APIKey            = "9769523b-ed52-415b-b95c-626b68754c49";
+// Cherwell RecID definitions
+const ReceivinObj       = "946d2d577152d0470a0cb04b07b3027024b375e869";
+const Rcv_CompanyName   = "946d2d6b0cd84516628db7461d8d565294cefedcb4";
+const Rcv_RecID         = "946d2d665b689b517180e64a0cbdf42a1891438a36";
+const Rcv_Template      = "946d2e9870d039dc9005f84d03bb341d7352416029";
+
+const ReceiveNSObj      = "946d7a3d753f9c790f350d4c119d56c75196aa88f8";
+const RcvNS_RecID       = "946d7a3d75a6c4c9b64802403db79e8d1ede4107b5";
+const RcvNS_RcvID       = "946d7a3d75c661f0c4e5b34efb8e07b6c05d992b88";
+const RcvNS_Desc        = "946d7a4d40f167f256d6094956b219d8bdcc737c08";
+const RcvNS_ProductID   = "946d7a3d75dd0f2c14f99e41f5864df9cdf208123c";
+
+const ProductObj        = "934083fa64f6888d862809493698e3ad4b709b9bb2";
+const Prd_Model         = "934083fb4518b1e5ec0cc1407289e2dc96c7949be4";
+const Prd_RecID         = "934083fa64de3be4d0097c42d399c3bb3b775e69d0";
+
+const ConfigObj         = "93ba37416cd565e13a2d2c4f2d8dcb287a17f7091f";
+const Cfg_RecID         = "9343f8800b00034d0b9b7446b791212b05bf2febf6";
+const Cfg_Model         = "9343f93bb67101661f6d8d49099a2df07c3200ef59";
+const Cfg_Serial        = "9343f88b85daf1bc3c475541a48526fdec9dc25960";
+
+const WebHookObj        = "946d7a73fbac30b2d70c124e8981e8f94e843655eb";
+const WH_Payload        = "946d7a78fef2de6377502b409384ff03e54f55c439";
+const WH_Name           = "946d7a7a37858602eba149404aaed207c80cd930cb";
+
